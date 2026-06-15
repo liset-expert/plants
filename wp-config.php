74d1ee10bc3efd04e8686a3b7048542f63616e98
liset-expert <yy30@expert.micro1.ai>
2026-06-15 18:37:27 -0300

    Update wp-config.php
    Removing duplicated information.

--- wp-config.php
+++ wp-config.php
@@ -89,20 +89,6 @@ $table_prefix = 'wp_';
  */
 define( 'WP_DEBUG', false );
 
-/**
- * For developers: WordPress debugging mode.
- *
- * Change this to true to enable the display of notices during development.
- * It is strongly recommended that plugin and theme developers use WP_DEBUG
- * in their development environments.
- *
- * For information on other constants that can be used for debugging,
- * visit the documentation.
- *
- * @link https://wordpress.org/support/article/debugging-in-wordpress/
- */
-define( 'WP_DEBUG', true );
-
 /* Add any custom values between this line and the "stop editing" line. */
 
 
